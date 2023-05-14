a527a5754984524448c81174f2b391c56f36a680
vicarb <vcardenasb@protonmail.com>
2023-05-13 20:33:39 -0400

    added user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.userModel.findOne({ username });
+  }
+
+  async create(username: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new this.userModel({ username, password: hashedPassword });
+    return newUser.save();
+  }
+}
